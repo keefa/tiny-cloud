90552b698600cfed9cd08b3f347b865d43bff656
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-04 09:39:04 +0100

    Add flash-color-64.sh for 64bit RPi 3

--- prepare-hardware/pi/flash-color-64.sh
+++ prepare-hardware/pi/flash-color-64.sh
@@ -8,7 +8,7 @@ if [ -z ${color} ]; then
 fi
 
 key="$(cat ~/.ssh/id_rsa.pub)"
-arch="arm64"
+arch="arm"
 sed "s|<color>|${color}|" user-data.template.yml | sed "s|<user>|${USER}|" | sed "s|<ssh-key>|${key}|" | sed "s|<arch>|${arch}|"> user-data.generated
 
-flash --hostname ${color} -u ./user-data.generated https://github.com/sealsystems/pi-gen/releases/download/v1.3.3/image_2017-02-22-pi-cloud-lite.zip
+flash --hostname ${color} -u ./user-data.generated https://github.com/sealsystems/image-builder-rpi64/releases/download/v1.0.0/hypriotos-rpi64-v1.0.0.img.zip
