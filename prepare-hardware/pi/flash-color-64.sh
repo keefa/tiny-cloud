c404ed940d3b651fa8ec5af6aac42f2f4660d04a
Stefan Scherer <scherer_stefan@icloud.com>
2017-04-09 12:30:27 +0200

    Back to Docker 1.13.1

--- prepare-hardware/pi/flash-color-64.sh
+++ prepare-hardware/pi/flash-color-64.sh
@@ -16,4 +16,4 @@ key="$(cat ~/.ssh/id_rsa.pub)"
 arch="arm64"
 sed "s|<color>|${color}|" user-data.template.yml | sed "s|<user>|${user}|" | sed "s|<ssh-key>|${key}|" | sed "s|<arch>|${arch}|"> user-data.generated
 
-flash --hostname ${color} -u ./user-data.generated https://github.com/sealsystems/image-builder-rpi64/releases/download/v1.4.0/hypriotos-rpi64-v1.4.0.img.zip
+flash --hostname ${color} -u ./user-data.generated https://github.com/sealsystems/image-builder-rpi64/releases/download/v1.0.1/hypriotos-rpi64-v1.0.1.img.zip
