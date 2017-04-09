c83578bb0b9a3b51441dcaab1e5ad8f59821e4ff
Stefan Scherer <scherer_stefan@icloud.com>
2017-04-09 11:18:13 +0200

    Flash SD image with Docker 17.04.0-ce

--- prepare-hardware/pi/flash-color.sh
+++ prepare-hardware/pi/flash-color.sh
@@ -11,4 +11,4 @@ key="$(cat ~/.ssh/id_rsa.pub)"
 arch="arm"
 sed "s|<color>|${color}|" user-data.template.yml | sed "s|<user>|${USER}|" | sed "s|<ssh-key>|${key}|" | sed "s|<arch>|${arch}|"> user-data.generated
 
-flash --hostname ${color} -u ./user-data.generated https://github.com/sealsystems/pi-gen/releases/download/v1.4.1/2017-03-08-pi-cloud-lite.zip
+flash --hostname ${color} -u ./user-data.generated https://github.com/sealsystems/pi-gen/releases/download/v1.5.0/2017-04-09-pi-cloud-lite.zip
