77d99533dd8e395bec53a1fc8e8b4f95128f91d2
Stefan Scherer <scherer_stefan@icloud.com>
2017-05-17 08:23:14 +0200

    Update tags

--- dockercon/pull-images.sh
+++ dockercon/pull-images.sh
@@ -4,9 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/nodes.config
 sshopts="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-#for node in $armNodes $intelNodes; do
-for node in 111 112 113 114 115 116; do
-  ssh $sshopts $user@$ipSubnet.$node docker pull stefanscherer/monitor:1.0.0
+for node in $armNodes $intelNodes; do
+  echo node $node
+  ssh $sshopts $user@$ipSubnet.$node docker pull stefanscherer/monitor:1.1.0
   ssh $sshopts $user@$ipSubnet.$node docker pull stefanscherer/whoami:1.1.0
   ssh $sshopts $user@$ipSubnet.$node docker pull stefanscherer/whoami:1.2.0
   ssh $sshopts $user@$ipSubnet.$node docker pull sealsystems/visualizer:1.1.5
